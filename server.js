d9f92d76df0f5a7fa68e8385d1fd2524b0982c4b
vivekUnico <vivek.unicoglobal@gmail.com>
2023-02-15 01:31:31 +0530

    done

--- server.js
+++ server.js
@@ -16,36 +16,36 @@ let obj = {
 app.get('/', async (req, res) => {
   try {
     let result = await SendEmail(obj);
-    return res.status(200).send({ message : "success", result });
+    return res.status(200).json({ message : "success" });
   } catch (error) {
-    res.status(500).send({ message : "failure", error });
+    res.status(500).json({ message : "failure", error });
   }
 });
 
 app.post('/', async (req, res) => {
   try {
     let result = await SendEmail(obj);
-    return res.status(200).send({ message : "success", result });
+    return res.status(200).json({ message : "success" });
   } catch (error) {
-    res.status(500).send({ message : "failure", error });
+    res.status(500).json({ message : "failure", error });
   }
 });
 
 app.put('/', async (req, res) => {
   try {
     let result = await SendEmail(obj);
-    return res.status(200).send({ message : "success", result });
+    return res.status(200).json({ message : "success" });
   } catch (error) {
-    res.status(500).send({ message : "failure", error });
+    res.status(500).json({ message : "failure", error });
   }
 });
 
 app.patch('/', async (req, res) => {
   try {
     let result = await SendEmail(obj);
-    return res.status(200).send({ message : "success", result });
+    return res.status(200).json({ message : "success" });
   } catch (error) {
-    res.status(500).send({ message : "failure", error });
+    res.status(500).json({ message : "failure", error });
   }
 });
 
