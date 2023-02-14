17822568afdf1fc385a30d2c996c76651862bf43
vivekUnico <vivek.unicoglobal@gmail.com>
2023-02-15 01:22:34 +0530

    done

--- SendEmail.js
+++ SendEmail.js
@@ -3,7 +3,6 @@ const { NOVU_API_KEY }  = process.env;
 const novu = new Novu(NOVU_API_KEY);
 
 exports.SendEmail = async (obj) => {
-  console.log(NOVU_API_KEY);
   const result = await novu.trigger('assignto', {
       to: {
         subscriberId: obj?.email.trim().toLowerCase(),
--- server.js
+++ server.js
@@ -16,36 +16,36 @@ let obj = {
 app.get('/', async (req, res) => {
   try {
     let result = await SendEmail(obj);
-    res.status(200).json({ message : "success", result });
+    return res.status(200).send({ message : "success", result });
   } catch (error) {
-    res.status(500).json({ message : "failure", error });
+    res.status(500).send({ message : "failure", error });
   }
 });
 
 app.post('/', async (req, res) => {
   try {
     let result = await SendEmail(obj);
-    res.status(200).json({ message : "success", result });
+    return res.status(200).send({ message : "success", result });
   } catch (error) {
-    res.status(500).json({ message : "failure", error });
+    res.status(500).send({ message : "failure", error });
   }
 });
 
 app.put('/', async (req, res) => {
   try {
     let result = await SendEmail(obj);
-    res.status(200).json({ message : "success", result });
+    return res.status(200).send({ message : "success", result });
   } catch (error) {
-    res.status(500).json({ message : "failure", error });
+    res.status(500).send({ message : "failure", error });
   }
 });
 
 app.patch('/', async (req, res) => {
   try {
     let result = await SendEmail(obj);
-    res.status(200).json({ message : "success", result });
+    return res.status(200).send({ message : "success", result });
   } catch (error) {
-    res.status(500).json({ message : "failure", error });
+    res.status(500).send({ message : "failure", error });
   }
 });
 
